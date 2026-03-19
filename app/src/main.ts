6df0c979652ed2e154d6db41e0f592bb9c389a7d
wz <wz@none.com>
2026-03-19 14:17:39 +0800

    增加代码编辑器

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
-import './assets/main.scss'
+import './assets/main.css'
 import App from './App.vue'
 import { router } from './routes'
+import { setupMonacoEditor } from './plugins/monaco-editor'
 
 const app = createApp(App)
 app.use(router)
+
+setupMonacoEditor()
 app.mount('#app')
--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { getInfo } from '../api'
+import { CodeEditor } from 'monaco-editor-vue3'
 
-const title = ref('')
-getInfo().then((d) => {
-  title.value = d.message
-})
+const code = ref(`SELECT * FROM Students s;`)
+const editorOptions = {
+  fontSize: 14,
+  minimap: { enabled: false },
+  automaticLayout: true,
+}
 </script>
 
 <template>
-  <div>{{ title }}</div>
+  <div class="h-full overflow-hidden grid grid-rows-[1fr_1fr]">
+    <div class="p-2">
+      <CodeEditor
+        v-model:value="code"
+        language="sql"
+        theme="vs-dark"
+        :options="editorOptions"
+      />
+    </div>
+    <div></div>
+  </div>
 </template>
 
 <style lang="scss"></style>
